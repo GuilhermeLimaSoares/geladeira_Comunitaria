5d5a4d7f9b7f189e49648ca7fab1295b45f977a7
Guilherme Soares <inm_gsoares@uolinc.com>
2019-01-15 18:55:18 -0200

    Projeto Web Geladeira Comunitária

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+let botao = document.querySelector("#index");
+
+function trocarCor(){
+    botao.style.backgroundColor = "green";
+}
+
+// function trocarBotao(){
+//     botaum.style.backgroundColor = "green";
+// }
+
+botao.onclick = trocarCor;
